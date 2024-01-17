5ecd64667816c7c59f4982d052628c94739fae18
jflan646 <47369954+jflan646@users.noreply.github.com>
2024-01-17 12:36:07 -0500

    Add files via upload

---
+++ workspace/Debug/src/Example.java
@@ -0,0 +1,12 @@
+// name date purpose
+public class Example {
+
+	public static void main(String[] args) {
+		int num1 = 12;
+		int num2= 44;
+		int sum = num1 + num2;
+		System.out.println("The sum is " + sum);
+
+	}
+
+}
